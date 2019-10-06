68fd1c5ca6dde798236d9eac2418d857a8e79a3e
Abhimanyu Pathania <pathania.abhimanyu@gmail.com>
2019-10-06 20:06:44 +0530

    Add about section on home page

--- src/views/Home/About/index.js
+++ src/views/Home/About/index.js
@@ -2,12 +2,35 @@ import React from 'react';
 
 import Title from '../../../components/Title';
 
+import img from '../../../images/defaultBcg.jpeg';
+import styles from './styles.module.css';
+
 const About = () => {
   return (
-    <div>
-      this is from about
+    <section className={styles.about}>
       <Title title="about" subtitle="us" />
-    </div>
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={img} alt="about image" />
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>explore the difference</h4>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis fugit minus omnis?
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis fugit minus omnis?
+          </p>
+          <button type="button" className="btn-primary">
+            read more
+          </button>
+        </article>
+      </div>
+    </section>
   );
 };
 
