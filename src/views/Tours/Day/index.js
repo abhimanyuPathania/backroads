9f3f4792d2feb3448903d9704ed619b4a88d257a
Abhimanyu Pathania <pathania.abhimanyu@gmail.com>
2019-10-14 15:11:27 +0530

    Add Day component to tours

--- src/views/Tours/Day/index.js
+++ src/views/Tours/Day/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaAngleDown } from 'react-icons/fa';
 
-const TourDay = () => {
-  return <div>This is day1</div>;
+import styles from './styles.module.css';
+
+const TourDay = ({ day, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  return (
+    <section className={styles.day}>
+      <h4>
+        {day}
+        <button className={styles.btn} onClick={() => setShowInfo(!showInfo)}>
+          <FaAngleDown />
+        </button>
+      </h4>
+      {showInfo && <p>{info}</p>}
+    </section>
+  );
 };
 
 export default TourDay;
